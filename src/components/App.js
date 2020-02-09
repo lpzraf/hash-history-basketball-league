e54f32dbd2ea7241516ac4b13f6b4a0b27c8253e
Rafael Lopez <lpz.raf@gmail.com>
2020-02-09 10:10:09 -0500

    It will add a 404 catch all

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home'
 import Players from './Players'
 import Teams from './Teams'
@@ -9,11 +9,14 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Navbar />
         <div>
-          <Route path='/' exact component={Home} />
-          <Route path='/players' component={Players} />
-          <Route path='/teams' component={Teams} />
+        <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/players' component={Players} />
+            <Route path='/teams' component={Teams} />
+            <Route render={() => <h1 className='text-center'>404!</h1>} />
+          </Switch>
         </div>
       </Router>
     )
