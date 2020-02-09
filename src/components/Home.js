ed20ac1ee4d3c5b70b2408442ccbba22a36a4a46
Rafael Lopez <lpz.raf@gmail.com>
2020-02-09 10:28:42 -0500

    It will set up the home route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { getTeamNames } from '../api'
+import TeamLogo from './TeamLogo'
+
 
 export default class Home extends Component {
+
+    state = {
+        teamNames: []
+    }
+
+    componentDidMount() {
+        getTeamNames()
+        .then((teamNames) => this.setState(() => ({
+            teamNames
+        })))
+    }
     render() {
+        const { teamNames } = this.state
         return (
             <div className='container'>
-                Home
+                <h1 className='large-header'>
+                    Hash Hisotry Basketball League
+                </h1>
+                <h3 className='header-text-center'>
+                    Select a team
+                </h3>
+                <div className='home-grid'>
+                    {teamNames.map((id) => (
+                        <Link key={id} to={`/${id}`}>
+                            <TeamLogo id={id} width='125px' />
+                        </Link>
+                    ))}
+                </div>
             </div>
         )
     }
